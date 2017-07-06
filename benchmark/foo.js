5115e7a453a9fe2e8fd6391b829ed4ef5c15c9f2
ttl <talrasha007@gmail.com>
2017-07-06 19:26:06 +0800

    benchmark for io

--- benchmark/foo.js
+++ benchmark/foo.js
@@ -7,8 +7,14 @@ suite('all', function () {
 
     bench('key gen', function () { env.genKey() });
     bench('encrypt', function () { key.encrypt([1, 2, 3, 4, 5, 6]); });
+
     bench('encrypt & decrypt', function () {
          const ctxt = key.encrypt([1, 2, 3, 4, 5, 6]);
          key.decrypt(ctxt);
+    });
+
+    bench('encrypt & decrypt buffer', function () {
+         const buf = key.encrypt([1, 2, 3, 4, 5, 6]).toBuffer();
+         key.decrypt(buf);
     });
 });
